3205cb1e8ed414565e81b28101fdcd5bff834b39
Yann <yannick@wolfpack-edition.com>
2021-03-23 21:04:14 +0300

    test POST, GET

--- backend/api/agency/editAgency.php
+++ backend/api/agency/editAgency.php
@@ -6,7 +6,7 @@
 
 // On envoie les headers
 header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: POST, GET");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Content-Type, Authorization, X-Requested-With");
 
