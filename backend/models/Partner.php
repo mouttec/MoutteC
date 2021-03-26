b9d6eb2e71df0f1de773e36b9d5b90a4e50d9763
Yann <yannick@wolfpack-edition.com>
2021-03-26 17:43:11 +0300

    correction R

--- backend/models/Partner.php
+++ backend/models/Partner.php
@@ -170,7 +170,7 @@ class Partner
             mailPartner = :mailPartner,        
             nameBilling = :nameBilling,
             siretPartner = :siretPartner,
-            numberAddrerssBilling = :numberAddressBilling,
+            numberAddressBilling = :numberAddressBilling,
             typeAddressBilling = :typeAddressBilling,
             nameAddressBilling = :nameAddressBilling,
             complementAddressBilling = :complementAddressBilling,
