f52aafdb1dc3837075e96fd8178e62f91a71ea3d
Yann <yannick@wolfpack-edition.com>
2021-04-26 07:33:14 +0300

    Faute de frappe dans le model Customer

--- backend/models/Customer.php
+++ backend/models/Customer.php
@@ -322,7 +322,7 @@ class Customer {
         $mailContent = '<p>Bonjour '. $messageContent->name .' !<br />Nous vous invitons à confirmer votre compte client chez MoutteC en créant votre mot de passe en cliquant sur 
                         <a href="https://mouttec.com/confirmAccount?key='.$messageContent->confirmationCode.'">ce lien</a>"<br /><br />
                         En cas de problème, copiez et collez ce lien dans votre navigateur préféré : <br />
-                        https://mouttec.com/confirmAccount?id='.$messageContent->id.'+key='.$messageContent->confirmationCode.'<br /><br />
+                        https://mouttec.com/confirmAccount?id='.$messageContent->id.'&key='.$messageContent->confirmationCode.'<br /><br />
                         Toute l\'équipe de <a href="https://mouttec.com">MoutteC</a> vous remercie de votre confiance !<br />';
         $customerHeaders =  'Reply-To: contact@mouttec.com' . "\r\n" .
                             'X-Mailer: PHP/' . phpversion() . "\r\n" .
